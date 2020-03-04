d106039f1a8c5dddc6784f27011c9376de136d82
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2020-03-04 11:37:53 +0100

    Add basic http2 tests

--- script/.travis-test.sh
+++ script/.travis-test.sh
@@ -8,117 +8,152 @@ SERVER=http://localhost:8080
 FILES=$ROOT/script/test
 HR="---------------------------------------------------------------------------"
 
-# fail / count
-STATUS="0 0"
+if [ ! -d tmp ]; then
+  mkdir tmp
+fi
+
+rm tmp/*
 
 add_result() {
-  is_success=$3
-  num_success=`expr $1 + $is_success`
-  num_tests=`expr $2 + 1`
-  echo "$num_success $num_tests"
+  echo $1 >&2
+  echo $1 >> tmp/results.log
 }
 
 get_failed() {
-  echo $1
+  echo `cat tmp/results.log | grep -v OK | wc -l`
 }
 
 get_count() {
-  echo $2
+  echo `cat tmp/results.log | wc -l`
 }
 
 expect_equal() {
-  status=$1
-  expected=$2
-  actual=$3
+  expected=$1
+  actual=$2
   if cmp $expected $actual; then
-    echo "OK" >&2
-    echo $(add_result $status 0)
+    add_result "OK"
   else
-    echo "FAIL (equality)" >&2
-    echo $(add_result $status 1)
+    add_result "FAIL (equality)"
   fi
 }
 
 expect_br_equal() {
-  status=$1
-  expected=$2
-  actual_br=$3
+  expected=$1
+  actual_br=$2
   if $BROTLI -dfk ./${actual_br}.br; then
-    echo $(expect_equal "$status" $expected $actual_br)
+    expect_equal $expected $actual_br
   else
-    echo "FAIL (decompression)" >&2
-    echo $(add_result $status 1)
+    add_result "FAIL (decompression)"
   fi
 }
 
-# Start server.
+################################################################################
+
+# Start default server.
 echo "Statring NGINX"
 $NGINX -c $ROOT/script/test.conf
 # Fetch vanilla 404 response.
-curl -s -o ./notfound.txt $SERVER/notfound
+curl -s -o tmp/notfound.txt $SERVER/notfound
+
+CURL="curl -s"
 
 # Run tests.
 echo $HR
 
 echo "Test: long file with rate limit"
-curl -s -H 'Accept-encoding: br' -o ./war-and-peace.br --limit-rate 300K $SERVER/war-and-peace.txt
-STATUS=$(expect_br_equal "$STATUS" $FILES/war-and-peace.txt ./war-and-peace)
+$CURL -H 'Accept-encoding: br' -o tmp/war-and-peace.br --limit-rate 300K $SERVER/war-and-peace.txt
+expect_br_equal $FILES/war-and-peace.txt tmp/war-and-peace
 
 echo "Test: compressed 404"
-curl -s -H 'Accept-encoding: br' -o ./notfound.br $SERVER/notfound
-STATUS=$(expect_br_equal "$STATUS" ./notfound.txt ./notfound)
+$CURL -H 'Accept-encoding: br' -o tmp/notfound.br $SERVER/notfound
+expect_br_equal tmp/notfound.txt tmp/notfound
 
 echo "Test: A-E: 'gzip, br'"
-curl -s -H 'Accept-encoding: gzip, br' -o ./ae-01.br $SERVER/small.txt
-STATUS=$(expect_br_equal "$STATUS" $FILES/small.txt ./ae-01)
+$CURL -H 'Accept-encoding: gzip, br' -o tmp/ae-01.br $SERVER/small.txt
+expect_br_equal $FILES/small.txt tmp/ae-01
 
 echo "Test: A-E: 'gzip, br, deflate'"
-curl -s -H 'Accept-encoding: gzip, br, deflate' -o ./ae-02.br $SERVER/small.txt
-STATUS=$(expect_br_equal "$STATUS" $FILES/small.txt ./ae-02)
+$CURL -H 'Accept-encoding: gzip, br, deflate' -o tmp/ae-02.br $SERVER/small.txt
+expect_br_equal $FILES/small.txt tmp/ae-02
 
 echo "Test: A-E: 'gzip, br;q=1, deflate'"
-curl -s -H 'Accept-encoding: gzip, br;q=1, deflate' -o ./ae-03.br $SERVER/small.txt
-STATUS=$(expect_br_equal "$STATUS" $FILES/small.txt ./ae-03)
+$CURL -H 'Accept-encoding: gzip, br;q=1, deflate' -o tmp/ae-03.br $SERVER/small.txt
+expect_br_equal $FILES/small.txt tmp/ae-03
 
 echo "Test: A-E: 'br;q=0.001'"
-curl -s -H 'Accept-encoding: br;q=0.001' -o ./ae-04.br $SERVER/small.txt
-STATUS=$(expect_br_equal "$STATUS" $FILES/small.txt ./ae-04)
+$CURL -H 'Accept-encoding: br;q=0.001' -o tmp/ae-04.br $SERVER/small.txt
+expect_br_equal $FILES/small.txt tmp/ae-04
 
 echo "Test: A-E: 'bro'"
-curl -s -H 'Accept-encoding: bro' -o ./ae-05.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-05.txt)
+$CURL -H 'Accept-encoding: bro' -o tmp/ae-05.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-05.txt
 
 echo "Test: A-E: 'bo'"
-curl -s -H 'Accept-encoding: bo' -o ./ae-06.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-06.txt)
+$CURL -H 'Accept-encoding: bo' -o tmp/ae-06.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-06.txt
 
 echo "Test: A-E: 'br;q=0'"
-curl -s -H 'Accept-encoding: br;q=0' -o ./ae-07.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-07.txt)
+$CURL -H 'Accept-encoding: br;q=0' -o tmp/ae-07.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-07.txt
 
 echo "Test: A-E: 'br;q=0.'"
-curl -s -H 'Accept-encoding: br;q=0.' -o ./ae-08.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-08.txt)
+$CURL -H 'Accept-encoding: br;q=0.' -o tmp/ae-08.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-08.txt
 
 echo "Test: A-E: 'br;q=0.0'"
-curl -s -H 'Accept-encoding: br;q=0.0' -o ./ae-09.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-09.txt)
+$CURL -H 'Accept-encoding: br;q=0.0' -o tmp/ae-09.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-09.txt
 
 echo "Test: A-E: 'br;q=0.00'"
-curl -s -H 'Accept-encoding: br;q=0.00' -o ./ae-10.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-10.txt)
+$CURL -H 'Accept-encoding: br;q=0.00' -o tmp/ae-10.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-10.txt
 
 echo "Test: A-E: 'br ; q = 0.000'"
-curl -s -H 'Accept-encoding: br ; q = 0.000' -o ./ae-11.txt $SERVER/small.txt
-STATUS=$(expect_equal "$STATUS" $FILES/small.txt ./ae-11.txt)
+$CURL -H 'Accept-encoding: br ; q = 0.000' -o tmp/ae-11.txt $SERVER/small.txt
+expect_equal $FILES/small.txt tmp/ae-11.txt
 
 echo "Test: A-E: 'bar'"
-curl -s -H 'Accept-encoding: bar' -o ./ae-12.txt $SERVER/small.html
-STATUS=$(expect_equal "$STATUS" $FILES/small.html ./ae-12.txt)
+$CURL -H 'Accept-encoding: bar' -o tmp/ae-12.txt $SERVER/small.html
+expect_equal $FILES/small.html tmp/ae-12.txt
 
 echo "Test: A-E: 'b'"
-curl -s -H 'Accept-encoding: b' -o ./ae-13.txt $SERVER/small.html
-STATUS=$(expect_equal "$STATUS" $FILES/small.html ./ae-13.txt)
+$CURL -H 'Accept-encoding: b' -o tmp/ae-13.txt $SERVER/small.html
+expect_equal $FILES/small.html tmp/ae-13.txt
+
+echo $HR
+echo "Stopping default NGINX"
+# Stop server.
+$NGINX -c $ROOT/script/test.conf -s stop
+
+################################################################################
+
+# Start default server.
+echo "Statring h2 NGINX"
+$NGINX -c $ROOT/script/test_h2.conf
+
+CURL="curl --http2-prior-knowledge -s"
+
+# Run tests.
+echo $HR
+
+echo "Test: long file with rate limit"
+$CURL -H 'Accept-encoding: br' -o tmp/h2-war-and-peace.br --limit-rate 300K $SERVER/war-and-peace.txt
+expect_br_equal $FILES/war-and-peace.txt tmp/h2-war-and-peace
+
+echo "Test: A-E: 'gzip, br'"
+$CURL -H 'Accept-encoding: gzip, br' -o tmp/h2-ae-01.br $SERVER/small.txt
+expect_br_equal $FILES/small.txt tmp/h2-ae-01
+
+echo "Test: A-E: 'b'"
+$CURL -H 'Accept-encoding: b' -o tmp/h2-ae-13.txt $SERVER/small.html
+expect_equal $FILES/small.html tmp/h2-ae-13.txt
+
+echo $HR
+echo "Stopping h2 NGINX"
+# Stop server.
+$NGINX -c $ROOT/script/test_h2.conf -s stop
+
+################################################################################
 
 # Report.
 
@@ -126,11 +161,6 @@ FAILED=$(get_failed $STATUS)
 COUNT=$(get_count $STATUS)
 echo $HR
 echo "Results: $FAILED of $COUNT tests failed"
-
-echo $HR
-echo "Stopping NGINX"
-# Stop server.
-$NGINX -c $ROOT/script/test.conf -s stop
 
 # Restore status-quo.
 cd $ROOT
